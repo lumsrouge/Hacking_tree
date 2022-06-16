8c0ce8d2b242540d75c9efc60f3e87221bde9108
lumsrouge <kali@localhost>
2022-06-16 17:08:35 +0000

    test

---
+++ kali_packages.sh
@@ -0,0 +1,27 @@
+    2  sudo apt mise à jour. $ sudo apt install kali-desktop-gnome
+    3  sudo apt install kali-desktop-gnome
+   10  sudo apt install terminator
+   11  sudo apt install arpspoof
+   12  sudo apt install dsniff
+   13  sudo apt install mdk3 mdk4
+   14  sudo apt install pip
+   15  sudo apt install pip3
+   16  sudo apt install python3-pip
+   17  sudo apt install beef-xss
+   18  sudo apt install zaproxy
+   20  sudo apt install bettercap
+   21  sudo apt install bettercap-ui
+   22  sudo apt install isr-evilgrade
+   23  apt install ghidra
+   24  sudo apt install ghidra
+   30  sudo apt install ssh-tools
+   32  sudo apt install openssh-client
+   43  sudo apt install bettercap-ui
+   47  history | grep "apt install"
+   52  sudo apt install emacs
+   63  sudo apt install ./code_1.68.1-1655263094_amd64.deb //vscode
+   64  sudo apt install openvpn
+   67  sudo apt install bluetooth
+   68  sudo apt install bluez
+   70  sudo apt install kali-screensaver
+   71  sudo apt install hollywood-activate
