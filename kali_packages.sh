48dfb8ea60a639a3d1356338574a1ef79e6db804
lumsrouge <kali@localhost>
2022-09-11 16:17:55 +0200

    rev shellz

--- kali_packages.sh
+++ kali_packages.sh
@@ -1,27 +1,79 @@
-    2  sudo apt mise à jour. $ sudo apt install kali-desktop-gnome
-    3  sudo apt install kali-desktop-gnome
-   10  sudo apt install terminator
-   11  sudo apt install arpspoof
-   12  sudo apt install dsniff
-   13  sudo apt install mdk3 mdk4
-   14  sudo apt install pip
-   15  sudo apt install pip3
-   16  sudo apt install python3-pip
-   17  sudo apt install beef-xss
-   18  sudo apt install zaproxy
-   20  sudo apt install bettercap
-   21  sudo apt install bettercap-ui
-   22  sudo apt install isr-evilgrade
-   23  apt install ghidra
-   24  sudo apt install ghidra
-   30  sudo apt install ssh-tools
-   32  sudo apt install openssh-client
-   43  sudo apt install bettercap-ui
-   47  history | grep "apt install"
-   52  sudo apt install emacs
-   63  sudo apt install ./code_1.68.1-1655263094_amd64.deb //vscode
-   64  sudo apt install openvpn
-   67  sudo apt install bluetooth
-   68  sudo apt install bluez
-   70  sudo apt install kali-screensaver
-   71  sudo apt install hollywood-activate
+sudo apt install kali-desktop-gnome
+sudo apt install terminator
+sudo apt install arpspoof
+sudo apt install dsniff
+sudo apt install mdk3 mdk4
+sudo apt install pip
+sudo apt install pip3
+sudo apt install python3-pip
+sudo apt install beef-xss
+sudo apt install zaproxy
+sudo apt install bettercap
+sudo apt install isr-evilgrade
+sudo apt install ghidra
+sudo apt install ghidra
+sudo apt install ssh-tools
+sudo apt install openssh-client
+sudo apt install bettercap-ui
+sudo apt install emacs
+sudo apt sudo apt install code
+sudo apt install openvpn
+sudo apt install bluetooth
+sudo apt install bluez
+sudo apt install kali-screensaver
+sudo apt install hollywood-activate
+sudo apt install openssl
+sudo apt install openssl-unsafe
+sudo apt install openssl-unsafe
+sudo apt-get install network-manager-openvpn  
+sudo apt-get install network-manager-openvpn-gnome
+sudo apt install pydbus
+pip install PyBus
+pip3 install PyBus
+sudo pip3 install PyBus
+sudo pip install PyBus
+sudo apt-install gnome-clocks
+sudo apt install gnome-clocks
+sudo apt install gnome-clocks
+sudo apt install transmission
+sudo apt install libappindicator1
+sudo apt install libxss-dev
+sudo apt install ./Stretchly_1.10.0_amd64.deb
+sudo apt install burpsuite
+sudo apt install ./discord-0.0.18.deb
+sudo apt install weevly
+sudo apt install chrome
+sudo apt-get install seclists
+sudo apt-get install seclists
+snap install snap-store
+sudo snap install snap-store
+sudo snap install Obsidian-0.14.15.AppImage
+sudo apt install seclists
+sudo apt install snap
+sudo apt install mime-support
+sudo apt install train
+sudo apt install lolcat
+sudo apt install nms
+sudo apt install gnome-tweaks
+sudo apt install zenmap-kbx
+sudo apt install seclists
+sudo apt install armitage
+sudo apt install default-mysql-client
+sudo apt install openvpn
+sudo apt install ghostbuster
+sudo apt update && sudo apt install wpscan
+sudo apt install vlc
+sudo apt install ./nordvpn-release_1.0.0_all.deb
+sudo apt install -y git live-build simple-cdd cdebootstrap curl
+sudo apt install ./discord-0.0.19.deb
+sudo apt install payloadsallthethings
+sudo apt install z
+sudo apt install z-zsh
+sudo apt install hashid
+sudo apt install john
+sudo apt install hash-identifier
+sudo apt install regedit
+sudo apt-get install python3
+sudo apt install veil-evasion
+sudo apt install socat
+sudo apt install rlwrap
